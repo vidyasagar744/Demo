38b709a49f8fb55b787153ea23b880bf059442ec
vidyasagar744 <9917004023@klu.ac.in>
2022-06-30 12:25:33 +0530

    Demo Restful APIs CRUD operations for Vibrant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CreateList from "./CreateList";
+import Lists from "./Lists";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      alldata: [],
+      singledata: {
+        title: "",
+        author: ""
+      }
+    };
+    this.getLists = this.getLists.bind(this);
+    this.getList = this.getList.bind(this);
+    this.createList = this.createList.bind(this);
+    this.updateList = this.updateList.bind(this);
+    this.deleteList = this.deleteList.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  getLists() {
+    this.setState({ loading: true }, () => {
+      fetch("http://localhost:3000/posts")
+        .then(res => res.json())
+        .then(result =>
+          this.setState({
+            loading: false,
+            alldata: result
+          })
+        )
+        .catch(console.log);
+    });
+  }
+
+  handleChange(event) {
+    var title = this.state.singledata.title;
+    var author = this.state.singledata.author;
+    if (event.target.name === "title") title = event.target.value;
+    else author = event.target.value;
+
+    this.setState({
+      singledata: {
+        title: title,
+        author: author
+      }
+    });
+  }
+
+  createList() {
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state.singledata)
+    }).then(
+      this.setState({
+        singledata: {
+          title: "",
+          author: ""
+        }
+      })
+    );
+  }
+
+  getList(event, id) {
+    this.setState(
+      {
+        singledata: {
+          title: "Loading...",
+          author: "Loading..."
+        }
+      },
+      () => {
+        fetch("http://localhost:3000/posts/" + id)
+          .then(res => res.json())
+          .then(result => {
+            this.setState({
+              singledata: {
+                title: result.title,
+                author: result.author ? result.author : ""
+              }
+            });
+          });
+      }
+    );
+  }
+
+  updateList(event, id) {
+    fetch("http://localhost:3000/posts/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state.singledata)
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          singledata: {
+            title: "",
+            author: ""
+          }
+        });
+        this.getLists();
+      });
+  }
+
+  deleteList(event, id) {
+    fetch("http://localhost:3000/posts/" + id, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          singledata: {
+            title: "",
+            author: ""
+          }
+        });
+        this.getLists();
+      });
+  }
+
+  render() {
+    const listTable = this.state.loading ? (
+      <span>Loading...Is usually slower than localhost...</span>
+    ) : (
+      <Lists
+        alldata={this.state.alldata}
+        singledata={this.state.singledata}
+        getList={this.getList}
+        updateList={this.updateList}
+        deleteList={this.deleteList}
+        handleChange={this.handleChange}
+      />
+    );
+    return (
+      <div className="container">
+        <span className="title-bar">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.getLists}
+          >
+            Get Lists
+          </button>
+          <CreateList
+            singledata={this.state.singledata}
+            createList={this.createList}
+            handleChange={this.handleChange}
+          />
+        </span>
+        <br />
+        {listTable}
+      </div>
+    );
+  }
 }
 
 export default App;
